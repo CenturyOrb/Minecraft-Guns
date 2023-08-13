80c664997e3ace984e3bfb2ba972a851c42621a2
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-08-12 22:53:19 -0400

    Fixed

--- src/main/java/com/rosed/minecraftguns/instance/Colt.java
+++ src/main/java/com/rosed/minecraftguns/instance/Colt.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class Colt extends Pistol {
@@ -24,17 +25,27 @@ public class Colt extends Pistol {
     public void shoot(Player player)   {
 
         player.sendMessage("You have shot your colt");
-        spawnParticleLine(player, Particle.FLAME, 100, 0.5);
+        spawnMovingParticleLine(player, Particle.FLAME, 10, 1, 3);
 
     }
 
-    public void spawnParticleLine(Player player, Particle particleType, int particleCount, double particleSpacing) {
+    public void spawnMovingParticleLine(Player player, Particle particleType, int particleCount, double particleSpacing, int updateInterval) {
         Vector direction = player.getEyeLocation().getDirection().normalize();
         Location particleLocation = player.getEyeLocation().clone();
 
         for (int i = 0; i < particleCount; i++) {
-            particleLocation.add(direction.clone().multiply(particleSpacing));
-            player.spawnParticle(particleType, particleLocation, 1);
+            final int iteration = i;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    particleLocation.add(direction.clone().multiply(particleSpacing));
+                    player.spawnParticle(particleType, particleLocation, 0, 0d, 0d, 0d);
+
+                    if (iteration >= particleCount - 1) {
+                        this.cancel();
+                    }
+                }
+            }.runTaskTimer(InstanceManager.INSTANCE.getMinecraftGuns(), updateInterval * i, updateInterval);
         }
     }
 
